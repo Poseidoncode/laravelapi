0b3d395bb63f71b416c4d343d174b6add84d9551
Poseidoncode <posidomhu@gmail.com>
2023-01-16 01:53:00 +0800

    addcrud

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\AnimalController;
+
+Route::apiResource('animal', AnimalController::class);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
